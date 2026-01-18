2149c0934960e28e642be813b2380dcf5a073ee3
Anton Abitharshan <aptharshan@gmail.com>
2026-01-18 17:30:34 +0530

    Initial commit with folder structure

---
+++ src/main/java/cloud/box/storage/CloudBoxApplication.java
@@ -0,0 +1,13 @@
+package cloud.box.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/box/storage/CloudBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.box.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
